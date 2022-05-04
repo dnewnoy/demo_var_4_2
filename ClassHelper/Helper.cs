3de2a7e7062b20ad642a5c7856210f009eabc1f4
dnewnoy <dnewnoy@mail.ru>
2022-05-04 20:19:37 +0300

    Session_2

---
+++ ClassHelper/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var4_2.ClassHelper
+{
+    internal class Helper
+    {
+        public static Entities DB = new Entities();
+    }
+}
